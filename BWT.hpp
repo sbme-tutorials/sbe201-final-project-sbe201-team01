733334319f02ef6bc22e8c0e5916230fee5f2fac
mernabobakr <merna.abobakr97@gmail.com>
2018-05-11 01:37:26 +0200

    sdsfdfd

--- BWT.hpp
+++ BWT.hpp
@@ -3,29 +3,20 @@
 #include <algorithm>
 #include <stdlib.h>
 #include "suffix.hpp"
-<<<<<<< HEAD
-
+namespace BWT
+{
 std::vector<char> LAST_COLUMN(std::string text)
 {
     int n = text.size();
     std::vector<char> BWT_arr;
 
     std::vector<int> SuffixesArray = SA::suffixarray(text);
-=======
-struct suffix
-{
-    int index;
-    std::string suff;
-};
-std::vector<char> LAST_COLUMN(std::string text)
-{
-    int n = text.size();
-    std::vector<char> BWT_arr(n) ;
-    std::vector<suffix> SuffixesArray = SA::getsuffixes(text);
->>>>>>> cfe9cd2c683b7dab17264afed69f65dfc39bb269
+    int x;
     for (int i = 0; i < n; i++)
     {
-        BWT_arr[i] = text[(SuffixesArray[i] + n - 1) % n];
+        x=SuffixesArray[i];
+        BWT_arr[i] = text[(x + n - 1) % n];
     }
     return BWT_arr;
+}
 }
