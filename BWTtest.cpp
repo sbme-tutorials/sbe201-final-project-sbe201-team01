3144bfa17d033b41cb2efd1974c914778c63aafe
mernabobakr <merna.abobakr97@gmail.com>
2018-05-15 21:53:18 +0200

    sdsfdfd

--- BWTtest.cpp
+++ BWTtest.cpp
@@ -5,8 +5,9 @@
 #include "suffix.hpp"
 int main()
 {
-    std::string x = "banana$";
+    std::string x = "GCGTGCCTGGTCA$";
     std::vector<BWT::BWTLF> v, z;
+
     z = BWT::bwt(x);
     v = BWT::firstcolumn(x);
     BWT::print(v);
