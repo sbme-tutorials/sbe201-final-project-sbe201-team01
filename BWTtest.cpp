3f4fd4dd4da647defdf23e42c23aad4ee36f979d
mernabobakr <merna.abobakr97@gmail.com>
2018-06-10 17:52:00 +0200

    sdsfdfd

--- BWTtest.cpp
+++ BWTtest.cpp
@@ -2,19 +2,27 @@
 
 #include <iostream>
 #include <string>
+#include<stack>
 #include <algorithm>
 #include "BWT.hpp"
 #include "suffix.hpp"
 int main()
 {
-    std::string x = "banana$";
-    std::vector<BWT::BWTLF> z,y;
+    std::string x = "panamabananas$";
+    std::vector<char> lastcolumn;
+    lastcolumn = BWT::last_column(x);
+    std::vector<BWT::BWTLF> fircolumn;
+    fircolumn = BWT::firstcolumn(x);
+    std::vector<int> LF;
+    LF = BWT::lasttofirst(fircolumn);
 
-    z = BWT::bwt(x);
-y=BWT::firstcolumn(x);
-std::vector<int>LF;
-LF=BWT::lasttofirst(y);
-    BWT::print(y);
-    BWT::printint(LF);
+std::stack<char>pattern;
+
+pattern.push('a');
+pattern.push('n');
+pattern.push('a');
+int nofoccurence;
+
+nofoccurence=BWT::findpattern(LF,lastcolumn,pattern);
+std::cout<<"the pattern is found "<<nofoccurence<<" times"<<std::endl;
 }
-
